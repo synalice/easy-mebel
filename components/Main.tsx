d58c348c3b81f7e7a1494ba8332c26395b710a44
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-11 20:43:13 +0300

    feat: add more responsivness

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,7 +4,7 @@ interface IMainProps {
 
 export default function Main({ children }: IMainProps) {
   return (
-    <div className="p-3 my-2 border border-gray-400">
+    <div className="p-3 sm:py-3 sm:px-12 md:py-3 md:px-28 my-2 border border-gray-400">
       <div className="rounded-lg p-2 flex flex-col items-center">
         {children}
       </div>
