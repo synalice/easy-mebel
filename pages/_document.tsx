f225955952722c7e05ffc631a8750277b9685141
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-11 20:43:33 +0300

    feat: move meta tag as NextJS wants it

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
           name="description"
           content="Мебельная фабрика в Балашихе. Кухни, Шкафы купе, Гардеробные, Гардеробные системы."
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
