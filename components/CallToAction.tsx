92d8bd88a9f6af4a8b380c53f9be01c5b0f9cbe4
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-11 20:42:31 +0300

    feat: reformat `<CallToAction />`

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -1,12 +1,11 @@
 import s from "./CallToAction.module.css";
 
-export default function CallToAction() {
+export default function CallToAction({ text, phone }: { text: string, phone: string }) {
   return (
     <div className="flex flex-col items-center justify-center my-3">
-      <button className={s.btn} name="ctaBtn">Позвонить нам</button>
-      <label htmlFor="ctaBtn">
-        <a href="tel:+7(926)218-66-68">+7(926)218-66-68</a>
-      </label>
+      <button className={s.btn} name="ctaBtn">
+        <a href={`tel:${phone}`}>{text}</a>
+      </button>
     </div>
   );
 }
