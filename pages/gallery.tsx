b32d6f6fd900ca57f586f7dae2a3ae5ce8fb5d5b
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-16 22:26:39 +0300

    feat: add `/gallery` page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,14 +1,34 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
+import ImageCarousel from "../components/ImageCarousel"
+import Article from "../components/Article";
+import Footer from "../components/Footer";
+import CallToAction from "../components/CallToAction";
 
 export default function Gallery() {
   return (
-    <Layout>
-      <Head>
-        <title>Easy Mebel | Галерея</title>
-      </Head>
-      <Header />
-    </Layout>
+    <>
+      <Layout>
+        <Head>
+          <title>Easy Mebel | Галерея</title>
+        </Head>
+
+        <Header />
+
+        <Article>
+          <h1>Наши работы</h1>
+          <p>Листайте в сторону, чтобы увидеть больше</p>
+          <ImageCarousel />
+        </Article>
+
+        {/* <Article>
+        <CallToAction text={"Позвонить нам"} phone={"+7(926)218-66-68"} />
+      </Article> */}
+
+      </Layout>
+
+      <Footer />
+    </>
   );
 }
