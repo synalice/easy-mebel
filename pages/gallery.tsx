191617876fa0fbb9ecc6a4286ae4cef9bc7b77ab
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-18 22:04:28 +0300

    feat: implement `getServerSideProps()` for `/gallery` page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,12 +1,25 @@
+import { InferGetServerSidePropsType } from "next";
+
+import * as fs from "fs";
+
 import Head from "next/head";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
 import ImageCarousel from "../components/ImageCarousel"
 import Article from "../components/Article";
 import Footer from "../components/Footer";
-import CallToAction from "../components/CallToAction";
 
-export default function Gallery() {
+export async function getServerSideProps() {
+  const slides: string[] = fs.readdirSync("./public/carousel/").map(file => "/carousel/" + file)
+
+  return {
+    props: {
+      slides: slides
+    }
+  }
+}
+
+export default function Gallery(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Layout>
@@ -19,13 +32,9 @@ export default function Gallery() {
         <Article>
           <h1>Наши работы</h1>
           <p>Листайте в сторону, чтобы увидеть больше</p>
-          <ImageCarousel />
+
+          <ImageCarousel slides={props.slides} />
         </Article>
-
-        {/* <Article>
-        <CallToAction text={"Позвонить нам"} phone={"+7(926)218-66-68"} />
-      </Article> */}
-
       </Layout>
 
       <Footer />
